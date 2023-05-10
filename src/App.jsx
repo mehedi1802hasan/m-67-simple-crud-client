4d6397f0f80644b9a7f88e43ade74125eb83b95d
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-05-10 21:35:49 +0600

    send data to server and form working properly doneeeeeee

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+ const handleAdd=(event)=>{
+  event.preventDefault();
+  const form=event.target;
+  const name=form.name.value;
+  const email=form.email.value;
+  const user={name,email};
+  console.log(user)
+  fetch('http://localhost:5000/users',{
+   method:"POST",
+   headers:{
+    'content-type':"application/json"
+   },
+   body:JSON.stringify(user)
+
+
+  })
+  .then(res=>res.json())
+  //.then(data=>console.log(data));
+  
+  .then(data => {
+    if(data.insertedId){
+      alert('User added successfully')
+      console.log(data)
+      form.reset()
+    }
+  })
+ }
   return (
     <>
       
       <h1>Crud simple</h1>
-      
+      <form onSubmit={handleAdd}>
+        <input type="text" name='name' /> <br />
+        <input type="text" name="email" id="" /><br />
+        <input type="submit" value="Add Value" id="" />
+      </form>
     </>
   )
 }
