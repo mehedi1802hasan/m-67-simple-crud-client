bdbc37378d9a778c447ed5df6f16a835cf37463c
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-05-11 10:55:23 +0600

    update done

--- src/Components/Update.jsx
+++ src/Components/Update.jsx
@@ -8,7 +8,24 @@ const Update = () => {
     const form=event.target;
     const name=form.name.value;
     const email=form.email.value;
-    console.log(name,email)
+   // console.log(name,email)
+    const updatedUser={name,email};
+    console.log(updatedUser)
+    fetch(`http://localhost:5000/users/${loadedUser._id}`,{
+      method:"PUT",
+      headers:{
+        'content-type':'application/json'
+      },
+      body:JSON.stringify(updatedUser)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data);
+      if(data.modifiedCount>0){
+        alert('great!!! updated successfully done')
+      }
+ 
+    })
   }
     return (
         <div>
